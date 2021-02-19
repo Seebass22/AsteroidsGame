21125d82929f76b6a1587b8782d167e260ac6fda
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-02-19 15:24:05 +0100

    initial commit

---
+++ Ship.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+
+var speed = 5
+var rotation_speed = 1.5
+var friction = 0.1
+var startPos = Vector2(100, 100)
+var maxSpeed = 600
+
+var velocity = Vector2()
+var rotation_dir = 0
+
+var xSize
+var ySize
+
+func _ready():
+	position = startPos
+	xSize = get_viewport_rect().size.x
+	ySize = get_viewport_rect().size.y
+
+func get_input():
+	rotation_dir = 0
+	if Input.is_action_pressed('turn_right'):
+		rotation_dir += 1
+	if Input.is_action_pressed('turn_left'):
+		rotation_dir -= 1
+	if Input.is_action_pressed('forward'):
+		velocity += Vector2(0, -speed).rotated(rotation)
+	if Input.is_action_just_pressed('shoot'):
+		print(get_viewport_rect().size)
+
+func _physics_process(delta):
+	get_input()
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
+	velocity -= Vector2(friction, friction)
+	constrain_position()
+	velocity = velocity.clamped(maxSpeed)
+
+func constrain_position():
+	if position.x > xSize:
+		position.x = 0
+	if position.x < 0:
+		position.x = xSize
+	if position.y > ySize:
+		position.y = 0
+	if position.y < 0:
+		position.y = ySize
