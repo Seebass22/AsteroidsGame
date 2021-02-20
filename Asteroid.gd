aebc4a480a66a24adb2943bbb459d0a7a7d3c451
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-02-20 14:01:06 +0100

    add background

--- Asteroid.gd
+++ Asteroid.gd
@@ -4,14 +4,12 @@ var speed = 250
 var minSpeed = 150
 var maxSpeed = 350
 
-var startPos = Vector2(800, 800)
 var velocity = Vector2()
 
 func _ready():
 	randomize()
 	rotation = randi() % 360
 	speed = rand_range(minSpeed, maxSpeed)
-	position = startPos
 	velocity = Vector2(0, 1).rotated(deg2rad(rotation)) * speed
 
 
