4606aa7c8863384f83e21084bf2c456037b203e3
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-02-20 17:39:40 +0100

    allow picking root note

--- Game.gd
+++ Game.gd
@@ -3,25 +3,24 @@ extends Node2D
 var notePitchScales = []
 var noteIndices = [0, 4, 7]
 
+# enum note {C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B}
+#            -9 -8  -7 -6  -5 -4 -3  -2 -1  0   1  2
 enum chordType {MAJ, MIN, DIM, AUG}
+
+export (int) var note = -8
 export (chordType) var currentChordType = chordType.MIN
 
 func _ready():
 	getNoteIndices()
+	adjustForNote()
 	getPitchScales()
 	setPitchScales()
 	playChord()
 
-	print(noteIndices[0])
-	print(noteIndices[1])
-	print(noteIndices[2])
-
-	print(notePitchScales[0])
-	print(notePitchScales[1])
-	print(notePitchScales[2])
 
 func getNoteForIndex(index):
 	return pow(2, (index as float / 12))
+
 
 func getNoteIndices():
 	match currentChordType:
@@ -34,17 +33,35 @@ func getNoteIndices():
 		chordType.AUG:
 			noteIndices = [0, 4, 8]
 
+
+func adjustForNote():
+	for i in range(noteIndices.size()):
+		noteIndices[i] += note
+
+
 func getPitchScales():
 	notePitchScales.append(getNoteForIndex(noteIndices[0]))
 	notePitchScales.append(getNoteForIndex(noteIndices[1]))
 	notePitchScales.append(getNoteForIndex(noteIndices[2]))
+
 
 func setPitchScales():
 	$Sound/Note1.pitch_scale = notePitchScales[0]
 	$Sound/Note2.pitch_scale = notePitchScales[1]
 	$Sound/Note3.pitch_scale = notePitchScales[2]
 
+
 func playChord():
 	$Sound/Note1.play()
 	$Sound/Note2.play()
 	$Sound/Note3.play()
+
+
+func debugPrintNoteInfo():
+	print(noteIndices[0])
+	print(noteIndices[1])
+	print(noteIndices[2])
+
+	print(notePitchScales[0])
+	print(notePitchScales[1])
+	print(notePitchScales[2])
