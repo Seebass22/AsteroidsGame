02aaf5e1918857368d6e9d7958daa1ead64b4dac
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-02-22 23:31:53 +0100

    add correct/wrong text

---
+++ src/Correct.gd
@@ -0,0 +1,18 @@
+extends Label
+
+export (float) var hide_time = 1.0
+
+func _ready():
+	GameEvents.connect("correct_asteroid", self, "_on_correct_asteroid_destroyed")
+	GameEvents.connect("incorrect_asteroid", self, "_on_incorrect_asteroid_destroyed")
+	set_text("")
+
+func _on_correct_asteroid_destroyed(ignored):
+	set_text("Correct!")
+	yield(get_tree().create_timer(hide_time), "timeout")
+	set_text("")
+
+func _on_incorrect_asteroid_destroyed(ignored):
+	set_text("Wrong!")
+	yield(get_tree().create_timer(hide_time), "timeout")
+	set_text("")
