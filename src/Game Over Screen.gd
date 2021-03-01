ac9512ea1867a07b6619de5f7e9fd0928a08dec1
Sebastian Thuemmel <sebastianthuemmel@yahoo.ca>
2021-03-01 01:58:51 +0100

    hide quit buttons on HTML5 build

--- src/Game Over Screen.gd
+++ src/Game Over Screen.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 export (NodePath) var _score_path
+export (NodePath) var _quit_path
 onready var _score = get_node(_score_path)
+onready var _quit_button = get_node(_quit_path)
 
 func _ready():
 	var percentage = (Results.final_score / Results.max_score as float) * 100
 	_score.set_text("%d / %d (%d%%)" % [Results.final_score, Results.max_score, percentage])
+
+	if OS.get_name() == "HTML5":
+		_quit_button.visible = false
 
 
 func _on_Restart_button_up():
@@ -17,5 +22,4 @@ func _on_Menu_button_up():
 
 
 func _on_Quit_button_up():
-	if OS.get_name() != "HTML5":
-		get_tree().quit()
+	get_tree().quit()
--- src/Main Menu.gd
+++ src/Main Menu.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+export (NodePath) var _quit_path
+onready var _quit_button = get_node(_quit_path)
+
+func _ready():
+	if OS.get_name() == "HTML5":
+		_quit_button.visible = false
+
+
 func _on_PlayButton_button_up():
 	get_tree().change_scene("res://Game.tscn")
 
@@ -9,5 +17,4 @@ func _on_HelpButton_button_up():
 
 
 func _on_QuitButton_button_up():
-	if OS.get_name() != "HTML5":
-		get_tree().quit()
+	get_tree().quit()
